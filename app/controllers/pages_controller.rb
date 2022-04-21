4180eb30e6ec78e364ed32bd5f7a78803c3142e7
Nachohk <ignacio.mlucero@gmail.com>
2022-04-20 21:45:47 -0400

    6topush

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  def index
+  def one
   end
 
-  def about
+  def two
   end
 
   def three
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/about'
+  get 'pages/one'
+  get 'pages/two'
   get 'pages/three'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#index"
+  root "pages#one"
 end
